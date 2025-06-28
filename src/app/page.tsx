b126a6e6be58d0439e1ed8264342afe7a5135696
Brian Denton <brian.w.denton@gmail.com>
2025-06-28 07:40:16 -0500

    Simplified Brutalist Landing Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import { BrutalistHero } from "@/components/brutalist-hero";
-import { BrutalistContent } from "@/components/brutalist-content";
 import { BrutalistFooter } from "@/components/brutalist-footer";
 
 export default function Home() {
 	return (
 		<div className="min-h-screen bg-black text-white overflow-x-hidden">
 			<BrutalistHero />
-			<BrutalistContent />
 			<BrutalistFooter />
 		</div>
 	);
